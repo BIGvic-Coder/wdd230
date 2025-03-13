6179b3369ae94d7267adaffd097181e7792a5653
victor <youremail@example.com>
2025-03-13 13:44:39 +0100

    Initial commit

--- scripts/getDates.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Dynamically set the current year in the footer
-  document.getElementById("year").textContent = new Date().getFullYear();
-
-  // Display the last modified date of the document
-  document.getElementById("lastModified").textContent =
-    "Last Modified: " + document.lastModified;
-});
